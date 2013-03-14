b193e6fe6d9676a3b4cffa806cfc962b212ab10a
David Jones <drj@pobox.com>
2013-03-14 21:46:41 +0000

    defixie

--- code/dbinv.py
+++ code/dbinv.py
@@ -3,6 +3,7 @@
 """Convert a downloaded inventry.lst into a sqlite file."""
 
 import os
+import re
 import sys
 
 import scraperwiki
@@ -14,11 +15,55 @@ def check_input():
         sys.stderr.write("Missing file: %s\nTry running code/getinv\n",
           INV_PATH)
 
+def scrape():
+    ocean = None
+    jasl = None
+    with open(INV_PATH) as f:
+      for row in f:
+        # Set ocean from a matching line, if possible
+        m = re.match(r'^\s+GENERAL\sINFORMATION.*\s+(\w+)\s+Ocean', row)
+        if m:
+            ocean = m.group(1)
+        # The data begins just after a line starting JASL, so detect that
+        if not jasl:
+            jasl = re.match(r'^JASL', row)
+            if jasl: continue
+        # Data ends with a blank line
+        if re.match(r'^\s*$', row):
+            jasl = False
+        if not jasl:
+            continue
+        # We're between a line starting JASL and a blank line, must
+        # be useful data.
+        d = {}
+        l = fixie('4 4 4 17 17 6 7 9 3 23', row)
+        (d['jaslid'], d['toga'], d['glos'], d['station'], d['country'],
+         d['lat'],  d['lon'], d['qcyears'], d['ci'], d['contributor']
+        ) = l
+        print d
+
+def fixie(fmt, row):
+    """Parse columns out of a fixie format row."""
+    res = []
+    c = 0
+    while fmt:
+        m = re.match(r'^[0-9]+', fmt)
+        if m:
+            i = int(m.group())
+            res.append(row[c:c+i])
+            fmt = fmt[len(m.group()):]
+            c += i
+        elif fmt[0] == ' ':
+            c += 1
+            fmt = fmt[1:]
+    return res
+
 def main(argv=None):
     import sys
     if argv is None:
         argv = sys.argv
     check_input()
+    scrape()
 
 if __name__ == '__main__':
     main()
